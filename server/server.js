40a1e9d80496bde13f928246d534c74b8f53c609
DoodleBob <riley.foust18@gmail.com>
2022-01-12 20:31:42 -0500

    added mutations to add friends and reactions

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 // import ApolloServer
 const {ApolloServer} = require('apollo-server-express');
 
+const {authMiddleware} = require('./utils/auth')
 const {typeDefs, resolvers} = require('./schemas')
 const db = require('./config/connection');
 
@@ -12,7 +13,7 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    // context: authMiddleware
+    context: authMiddleware
   });
 
   await server.start();
